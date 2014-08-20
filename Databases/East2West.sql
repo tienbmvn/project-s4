77f8a3b9622b83ba35f0a8585225155e439589db
Bui Manh Tien <tienbmvn@gmail.com>
2014-08-20 20:17:54 +0700

    header home

--- Databases/East2West.sql
+++ Databases/East2West.sql
@@ -16,14 +16,22 @@ GO
 CREATE TABLE PackageTours
 (
 	TouristId int identity CONSTRAINT pk_TouristId primary key, 
+	TouristName nvarchar(100) not null,
 	TouristPlace nvarchar(100) not null,
-	Accommodation nvarchar(300) not null,
-	TouristFood nvarchar(300) not null,
-	LocalTravel text not null,
+	TouristDescription nvarchar(100) not null,
 	TourisPrice float not null CHECK (TourisPrice>0),
 	TourisDuration nvarchar(50) not null,
 	ImagesTouris text,
 	TypeTouris nvarchar(5) not null CONSTRAINT fk_TypeTouris FOREIGN KEY (TypeTouris) REFERENCES TypePackage(TypeTourisId),
+
 )
 GO
-
+CREATE TABLE PackageInfo
+(
+	InfoId int identity CONSTRAINT pk_InfoId primary key,
+	Accommodation nvarchar(300) not null,
+	TouristFood nvarchar(300) not null,
+	LocalTravel text not null,
+	ImagesTouris text,
+	PackageTouristId int not null CONSTRAINT fk_TouristId FOREIGN KEY (PackageTouristId) REFERENCES PackageTours(TouristId)
+)
--- Databases/sp_East2West.sql
+++ Databases/sp_East2West.sql
@@ -9,14 +9,23 @@ insert into dbo.TypePackage values(@TypeTourisId,@PackageName)
 GO
 /*==================================== PackageTours Table Procedure ======================================*/
 create proc sp_PackageTours
+@TouristName nvarchar(100),
 @TouristPlace nvarchar(100),
-@Accommodation nvarchar(300),
-@TouristFood nvarchar(300),
-@LocalTravel text,
+@TouristDescription nvarchar(100),
 @TourisPrice float,
 @TourisDuration nvarchar(50),
 @ImagesTouris text,
 @TypeTouris nvarchar(5)
 As
-insert into dbo.PackageTours values(@TouristPlace,@Accommodation,@TouristFood,@LocalTravel,@TourisPrice,@TourisDuration,@ImagesTouris,@TypeTouris)
+insert into dbo.PackageTours values(@TouristName,@TouristPlace,@TouristDescription,@TourisPrice,@TourisDuration,@ImagesTouris,@TypeTouris)
+GO
+/*==================================== InfoPackageTours Table Procedure ======================================*/
+create proc sp_InfoPackageTours
+@Accommodation nvarchar(300),
+@TouristFood nvarchar(300),
+@LocalTravel text,
+@ImagesTouris text,
+@PackageTouristId int
+As
+insert into dbo.PackageInfor values(@Accommodation,@TouristFood,@LocalTravel,@ImagesTouris,@PackageTouristId)
 GO
